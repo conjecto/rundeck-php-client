a152129d326b49ca644f95e691f9a412190041ea
erwan <erwan.delahaye@conjecto.com>
2016-10-06 12:37:47 +0200

    - reformat a little - add id and group call for getting jobs - add run job - add options on export jobs - return response on import jobs

--- src/Rundeck/RundeckClient.php
+++ src/Rundeck/RundeckClient.php
@@ -7,13 +7,15 @@ use Rundeck\Api as api;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
 
-class RundeckClient {
+class RundeckClient
+{
 
 	protected $client;
 	protected $jsession;
 	protected $majorMinorVersion;
 
-	public function __construct($url, $user, $pass) {
+	public function __construct($url, $user, $pass)
+	{
 		$this->client = new Client([
 			'base_url' => $url
 		]);
@@ -38,11 +40,13 @@ class RundeckClient {
 		$this->majorMinorVersion = (int)($version[0] . $version[1]);
 	}
 
-	public function getHttpClient() {
+	public function getHttpClient()
+	{
 		return $this->client;
 	}
 
-	public function getProjects() {
+	public function getProjects()
+	{
 		$resp = $this->client->get('/api/1/projects', [
 			'cookies' => ['JSESSIONID' => $this->jsession]
 		]);
@@ -52,17 +56,39 @@ class RundeckClient {
 		return (new api\ProjectApiMapper)->getAllFromEncoded($data['projects']['project']);
 	}
 
-	public function getJobs($projectName) {
-		$resp = $this->client->get("/api/1/jobs?project=$projectName", [
+	public function getJobs($projectName, $jobId = null, $groupName = null)
+	{
+		$jobPath = $groupPath = '';
+		if ($jobId) {
+			$jobPath .= '&idlist=' . $jobId;
+		}
+		if ($groupName) {
+			$groupPath .= '&groupPath=' . $groupName;
+		}
+		$resp = $this->client->get("/api/1/jobs?project=$projectName$jobPath$groupPath", [
 			'cookies' => ['JSESSIONID' => $this->jsession]
 		]);
 
 		$data = $this->decodeResponse($resp);
 
-		return (new api\JobApiMapper)->getAllFromEncoded($data['jobs']['job']);
+		if(!isset($data['jobs']['job'])) return false;
+
+		return (new api\JobApiMapper)->getAllFromEncoded(array($data['jobs']['job']));
 	}
 
-	public function createProject($name) {
+	public function runJob($id)
+	{
+		$resp = $this->client->get("/api/1/job/$id/run", [
+			'cookies' => ['JSESSIONID' => $this->jsession]
+		]);
+
+		$data = $this->decodeResponse($resp);
+
+		return (new api\JobApiMapper)->getAllFromEncoded(array($data['executions']['execution']['job']));
+	}
+
+	public function createProject($name)
+	{
 		if ($this->majorMinorVersion < 21) {
 			throw new \Exception('Unsupported feature. Upgrade to Rundeck 2.1.+');
 		}
@@ -77,7 +103,8 @@ class RundeckClient {
 		}
 	}
 
-	public function deleteProject($name) {
+	public function deleteProject($name)
+	{
 		if ($this->majorMinorVersion < 21) {
 			throw new \Exception('Unsupported feature. Upgrade to Rundeck 2.1.+');
 		}
@@ -91,31 +118,40 @@ class RundeckClient {
 		}
 	}
 
-	public function exportJobs($projectName) {
-		return $this->client->get("/api/10/jobs/export?project=$projectName", [
+	public function exportJobs($projectName, $jobId = null)
+	{
+		$jobPath = '';
+		if ($jobId) {
+			$jobPath .= '&idlist=' . $jobId;
+		}
+		return $this->client->get("/api/10/jobs/export?project=$projectName$jobPath", [
 			'cookies' => ['JSESSIONID' => $this->jsession]
 		])->xml()->asXML();
 	}
 
-	public function importJobs($projectName, $xml) {
+	public function importJobs($projectName, $xml)
+	{
 		try {
-			$this->client->post("/api/10/jobs/import", [
-				'cookies'      => ['JSESSIONID' => $this->jsession],
-				'headers'      => ['Content-Type' => 'application/x-www-form-urlencoded'],
-				'body'         => [
-					'project'    => $projectName, 
+			$response = $this->client->post("/api/10/jobs/import", [
+				'cookies' => ['JSESSIONID' => $this->jsession],
+				'headers' => ['Content-Type' => 'application/x-www-form-urlencoded'],
+				'body' => [
+					'project' => $projectName,
 					'uuidOption' => 'preserve',
-					'xmlBatch'   => $xml, 
+					'xmlBatch' => $xml,
 				],
 			]);
+			$data = $this->decodeResponse($response);
+			return $data;
 		} catch (ClientException $e) {
 			// ignore, assume it's a 400 and the project already exists
 		}
 	}
 
-	private function decodeResponse($resp) {
-		$xml   = simplexml_load_string($resp->xml()->asXml());
-		$json  = json_encode($xml);
+	private function decodeResponse($resp)
+	{
+		$xml = simplexml_load_string($resp->xml()->asXml());
+		$json = json_encode($xml);
 
 		return json_decode($json, true);
 	}
